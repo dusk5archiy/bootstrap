fcaad2c32fc3ca494e9eb5c620ae4a8502e01e9c
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-04-19 17:55:20 +0700

    fix sudo

--- src/s7sys/settings/get/lazygit.sh
+++ src/s7sys/settings/get/lazygit.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 OS="$(. /etc/os-release && echo $ID)"
+
+if [[ "$OS" == "msys2" || "$HOME" == "/root" ]]; then
+  sudo=""
+else
+  sudo="sudo"
+fi
+
 case "$OS" in
 ubuntu)
   LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
   LAZYGIT_ARCH=$(uname -m | sed -e 's/aarch64/arm64/')
   curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_${LAZYGIT_ARCH}.tar.gz"
   tar xf lazygit.tar.gz lazygit
-  sudo install lazygit -D -t /usr/local/bin/
+  $sudo install lazygit -D -t /usr/local/bin/
   ;;
 esac
