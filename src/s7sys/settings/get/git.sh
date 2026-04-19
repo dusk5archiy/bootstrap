6362c53b31a82a6e2a0eeb653e1aa052483638ca
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-04-19 18:02:09 +0700

    add git

--- src/s7sys/settings/get/git.sh
+++ src/s7sys/settings/get/git.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-pkg-download.sh "git" \
-  "https://github.com/git-for-windows/git/releases/download/v2.53.0.windows.3/PortableGit-2.53.0.3-64-bit.7z.exe"
+OS="$(. /etc/os-release && echo $ID)"
+case "$OS" in
+msys2)
+  pkg-download.sh "git" \
+    "https://github.com/git-for-windows/git/releases/download/v2.53.0.windows.3/PortableGit-2.53.0.3-64-bit.7z.exe"
+  ;;
+esac
