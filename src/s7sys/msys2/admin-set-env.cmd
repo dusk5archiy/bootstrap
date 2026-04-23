f0050c4560931ab2f5c0ea6a733ac5ba336c9ce9
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-04-23 17:32:27 +0700

    fix: fix outdated paths

--- src/s7sys/msys2/admin-set-env.cmd
+++ src/s7sys/msys2/admin-set-env.cmd
@@ -1,6 +1,6 @@
 @echo off
 for %%I in ("%~dp0..\..") do set "CWD=%%~fI"
-set "APP_PATH=%CWD%\s7sys\msys2\wezterm-set-env.cmd"
+set "APP_PATH=%CWD%\s7sys\msys2\normal-set-env.cmd"
 
 powershell -Command "Start-Process -FilePath '%APP_PATH%' -Verb RunAs"
 
