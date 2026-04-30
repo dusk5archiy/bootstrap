116b3781fb31ab91bcbe729caec4909f8eab9dee
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-04-30 23:21:41 +0700

    add equals 1

--- src/s7sys/settings/config/nvim/lua/config/options.lua
+++ src/s7sys/settings/config/nvim/lua/config/options.lua
@@ -2,7 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-if vim.fn.has("win32") then
+if vim.fn.has("win32") == 1 then
 	vim.o.shellslash = false
 end
 vim.opt.colorcolumn = "80"
