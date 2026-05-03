2add448e217bf9fe90869f436460bb12dd13eec1
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-05-03 23:17:29 +0700

    add vscode remote containers

--- src/s7sys/settings/scripts/code.init.sh
+++ src/s7sys/settings/scripts/code.init.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 extensions=(
-  "ms-python.python"            # Python
-  "ms-toolsai.jupyter"          # Jupyter Notebook
-  "anthropic.claude-code"       # Claude Code
-  "tomoki1207.pdf"              # PDF Viewer
-  "asvetliakov.vscode-neovim"   # Neovim
-  "ms-vscode-remote.remote-wsl" # WSL
+  "ms-python.python"                   # Python
+  "ms-toolsai.jupyter"                 # Jupyter Notebook
+  "anthropic.claude-code"              # Claude Code
+  "tomoki1207.pdf"                     # PDF Viewer
+  "asvetliakov.vscode-neovim"          # Neovim
+  "ms-vscode-remote.remote-wsl"        # WSL
+  "ms-vscode-remote.remote-containers" # Dev Containers
 )
 
 for ext in "${extensions[@]}"; do
