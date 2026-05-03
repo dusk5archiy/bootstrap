4d1c68b84a260269055c630f7056d8620da95e4b
Dusk Sarchiy <dusqdn.eml@gmail.com>
2026-05-03 23:21:45 +0700

    change vscode extension list

--- src/s7sys/settings/scripts/code.init.sh
+++ src/s7sys/settings/scripts/code.init.sh
@@ -3,11 +3,12 @@
 extensions=(
   "ms-python.python"                   # Python
   "ms-toolsai.jupyter"                 # Jupyter Notebook
-  "anthropic.claude-code"              # Claude Code
   "tomoki1207.pdf"                     # PDF Viewer
   "asvetliakov.vscode-neovim"          # Neovim
   "ms-vscode-remote.remote-wsl"        # WSL
   "ms-vscode-remote.remote-containers" # Dev Containers
+  "Catppuccin.catppuccin-vsc"          # Catppuccin Theme
+  "Catppuccin.catppuccin-vsc-icons"    # Catppuccin Icons
 )
 
 for ext in "${extensions[@]}"; do
