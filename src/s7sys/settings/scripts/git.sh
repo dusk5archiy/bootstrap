fde37d7b8dd21f38a545cb2c2250ac5fa66771ed
Duc Nq <ducnq@dn.soc.one>
2026-06-05 21:22:21 +0700

    add git add new key

--- src/s7sys/settings/scripts/git.sh
+++ src/s7sys/settings/scripts/git.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-read -p "Name: " name_
 read -p "Email: " email_
 
-touch "$HOME/.gitconfig"
-git config --global user.name "$name_"
-git config --global user.email "$email_"
-git config --global safe.directory '*'
-git config --global init.defaultbranch main
-git config --global credential.helperselector.selected "<no helper>"
-git config --global --list
+if [[ -z "$1" ]]; then
+  read -p "Name: " name_
+  touch "$HOME/.gitconfig"
+  git config --global user.name "$name_"
+  git config --global user.email "$email_"
+  git config --global safe.directory '*'
+  git config --global init.defaultbranch main
+  git config --global credential.helperselector.selected "<no helper>"
+  git config --global --list
+fi
+
+ssh-keygen -t ed25519 -C "$email_" -f "$HOME/.ssh/id_ed25519"
+
+eval "$(ssh-agent -s)"
+ssh-add "$HOME/.ssh/id_ed25519"
+cat "$HOME/.ssh/id_ed25519.pub"
